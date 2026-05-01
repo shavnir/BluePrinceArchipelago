c66181073c1bd9c1ba497bd7d0675a86fec70a10
Yascob99 <yascob09@gmail.com>
2026-04-30 21:06:53 -0600

    Added Polyfill.

---
+++ BluePrinceArchipelago/PolyFill.cs
@@ -0,0 +1,25 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+//Poly Fill to fix nullable build errors on Unity 6
+#pragma warning disable
+
+namespace System.Runtime.CompilerServices;
+
+using System.ComponentModel;
+
+[EditorBrowsable(EditorBrowsableState.Never)]
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Event | AttributeTargets.Parameter | AttributeTargets.ReturnValue | AttributeTargets.GenericParameter, Inherited = false)]
+internal sealed class NullableAttribute : Attribute
+{
+    public readonly byte[] NullableFlags;
+
+    public NullableAttribute(byte value)
+    {
+        this.NullableFlags = [value];
+    }
+
+    public NullableAttribute(byte[] value)
+    {
+        this.NullableFlags = value;
+    }
+}
